585bc727d145023e379e92c0aed6028c469d09e1
Hossam <hossamabdelhady000@gmail.com>
2026-01-09 11:09:28 +0200

    Refactor splash screen to use animation and update theme to dark mode

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -2,13 +2,62 @@ import 'package:bookly_app/core/helpers/constants/assets_constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with TickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<double> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 3),
+    );
+    slidingAnimation = Tween<double>(
+      begin: 0,
+      end: 1,
+    ).animate(animationController);
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: SvgPicture.asset(AssetsConstants.mainLogo, width: 200),
+      child: FadeAnimaitionBuilder(slidingAnimation: slidingAnimation),
+    );
+  }
+}
+
+class FadeAnimaitionBuilder extends StatelessWidget {
+  const FadeAnimaitionBuilder({super.key, required this.slidingAnimation});
+
+  final Animation<double> slidingAnimation;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: slidingAnimation,
+      builder: (context, _) {
+        return FadeTransition(
+          alwaysIncludeSemantics: true,
+          opacity: slidingAnimation,
+          child: SvgPicture.asset(AssetsConstants.mainLogo, width: 200),
+        );
+      },
     );
   }
 }
