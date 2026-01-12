d00c7923bace3cd4e74b6d89b4fffd444dfdfe10
Hossam <hossamabdelhady000@gmail.com>
2026-01-12 06:33:19 +0200

    feat: Update text styles and integrate Google Fonts for improved typography

--- lib/core/utils/app_text_styles.dart
+++ lib/core/utils/app_text_styles.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 abstract class AppTextStyles {
   static const TextStyle font18WhiteSemiBold = TextStyle(
     fontSize: 18,
-    color: Colors.white,
-    fontWeight: FontWeight.w600,
+    fontWeight: FontWeight.w700,
   );
 }
