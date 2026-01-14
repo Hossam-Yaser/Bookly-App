e71a762d705a36c09b0d0b0a83caffd5671dcfd0
Hossam <hossamabdelhady000@gmail.com>
2026-01-14 08:51:09 +0200

    feat: Refactor BestsellerbooksCubit and FuturedbooksCubit to include HomeRepo dependency and implement fetch methods

--- lib/features/home/presentation/view_model/bestsellerbooks_cubit/bestsellerbooks_cubit.dart
+++ lib/features/home/presentation/view_model/bestsellerbooks_cubit/bestsellerbooks_cubit.dart
@@ -1,9 +1,26 @@
-import 'package:bloc/bloc.dart';
 import 'package:bookly_app/features/home/data/models/book_response_model/book_response_model.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'bestsellerbooks_state.dart';
 
 class BestsellerbookscubitCubit extends Cubit<BestsellerbookscubitState> {
-  BestsellerbookscubitCubit() : super(BestsellerbookscubitInitial());
+  final HomeRepo _homeRepo;
+  BestsellerbookscubitCubit(this._homeRepo)
+    : super(BestsellerbookscubitInitial());
+
+  Future<void> fetchBestSellerBooks() async {
+    emit(BestsellerbookscubitLoading());
+
+    var result = await _homeRepo.fetchBestSellerBooks();
+    result.fold(
+      (booksList) {
+        emit(BestsellerbookscubitSuccess(booksList));
+      },
+      (failure) {
+        emit(BestsellerbookscubitFailure(failure.errorMessage));
+      },
+    );
+  }
 }
--- lib/features/home/presentation/view_model/futured_books_cubit/futuredbooks_cubit.dart
+++ lib/features/home/presentation/view_model/futured_books_cubit/futuredbooks_cubit.dart
@@ -1,9 +1,25 @@
 import 'package:bookly_app/features/home/data/models/book_response_model/book_response_model.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'futuredbooks_state.dart';
 
 class FuturedbooksCubit extends Cubit<FuturedbooksState> {
-  FuturedbooksCubit() : super(FuturedbooksInitial());
+  final HomeRepo _homeRepo;
+  FuturedbooksCubit(this._homeRepo) : super(FuturedbooksInitial());
+
+  Future<void> fetchFuturedBooks() async {
+    emit(FuturedbooksLoading());
+    var result = await _homeRepo.fetchFuturedBooks();
+
+    result.fold(
+      (booksList) {
+        emit(FuturedbooksSuccess(booksList));
+      },
+      (failure) {
+        emit(FuturedbooksFailure(failure.errorMessage));
+      },
+    );
+  }
 }
