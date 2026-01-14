aeae99caf19a119d196b90cbf41489f6af4efc26
Hossam <hossamabdelhady000@gmail.com>
2026-01-14 10:21:07 +0200

    feat: Add dependency injection setup and register ApiService and HomeRepoImpl

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:bookly_app/core/helpers/constants/colors_constants.dart';
 import 'package:bookly_app/core/utils/app_router.dart';
+import 'package:bookly_app/core/utils/dependancy_injection.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  setupDependancyInjec();
   runApp(const BooklyApp());
 }
 
